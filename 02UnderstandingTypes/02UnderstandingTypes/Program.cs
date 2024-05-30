e965132e21f68feec81caad4e715d38331d848d2
Dandanlearning <123603821+Dandanlearning@users.noreply.github.com>
2024-05-30 17:46:33 -0400

    Add files via upload

---
+++ 02UnderstandingTypes/02UnderstandingTypes/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _02UnderstandingTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string format = "{0,-10} | {1,-15} | {2,-37} | {3,-37}";
+            Console.WriteLine(string.Format(format, "DataType", "Number of Bytes", "Min Value", "Max Value"));
+            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.WriteLine(string.Format(format, "sbyte", sizeof(sbyte), sbyte.MinValue, sbyte.MaxValue));
+            Console.WriteLine(string.Format(format, "byte", sizeof(byte), byte.MinValue, byte.MaxValue));
+            Console.WriteLine(string.Format(format, "short", sizeof(short), short.MinValue, short.MaxValue));
+            Console.WriteLine(string.Format(format, "ushort", sizeof(ushort), ushort.MinValue, ushort.MaxValue));
+            Console.WriteLine(string.Format(format, "int", sizeof(int), int.MinValue, int.MaxValue));
+            Console.WriteLine(string.Format(format, "uint", sizeof(uint), uint.MinValue, uint.MaxValue));
+            Console.WriteLine(string.Format(format, "long", sizeof(long), long.MinValue, long.MaxValue));
+            Console.WriteLine(string.Format(format, "ulong", sizeof(ulong), ulong.MinValue, ulong.MaxValue));
+            Console.WriteLine(string.Format(format, "float", sizeof(float), float.MinValue, float.MaxValue));
+            Console.WriteLine(string.Format(format, "double", sizeof(double), double.MinValue, double.MaxValue));
+            Console.WriteLine(string.Format(format, "decimal", sizeof(decimal), decimal.MinValue, decimal.MaxValue));
+            
+            TimeConvert timeConvert = new();
+            //int x = 81474*83648;
+            //Console.WriteLine(x);
+            //int x = 5;
+            //int y = x++;
+            Console.WriteLine("What number do you want to convert, starting at centry?");
+            int n = int.Parse(Console.ReadLine());
+            timeConvert.TimeResult(n);
+        }
+    }
+}
---
+++ 02UnderstandingTypes/02UnderstandingTypes/TimeConvert.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02UnderstandingTypes
+{
+    public class TimeConvert
+    {
+        public int year;
+        public float day;
+        public float hour;
+        public float minute;
+        public double second;
+        public double millisecond;
+        public decimal microsecond;
+        public decimal nanosecond;
+
+        public void TimeResult(int cen)
+        {
+            year = 100 * cen;
+            day = (365.242f * year);
+            if (day % 1 != 0)
+            {
+                int t = (int)day;
+                day = (float)t;
+            }
+            hour = 24 * day;
+            minute = 60 * hour;
+            second = (double)minute * 60;
+            millisecond = second * 1000;
+            microsecond = (decimal)millisecond * 1000;
+            nanosecond = microsecond * 1000;
+            Console.WriteLine($"{cen} centuries = {year} years  = {day} days = {hour} hours = " +
+                $" {minute} minutes = {second} seconds = {millisecond} milliseconds = {microsecond} microseconds =" +
+                $" {nanosecond} nanosecond");
+
+        }
+    }
+}
